5c649b47a847013e53cb4bc921e0e6fbc1004d7b
mohammeddl <mohammed_daali2@um5.ac.ma>
2024-09-30 23:43:39 +0100

    EM-1 fix model

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -5,14 +5,14 @@ public class Employee {
     private String email;
     private String post;
     private String phone;
-    private String addresdepartments;
+    private String position;
 
-    public Employee(String name, String email, String post, String phone, String addresdepartments) {
+    public Employee(String name, String email, String post, String phone, String position) {
         this.name = name;
         this.email = email;
         this.post = post;
         this.phone = phone;
-        this.addresdepartments = addresdepartments;
+        this.position = position;
     }
 
     public String getName() {
@@ -26,17 +26,35 @@ public class Employee {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPost() {
         return post;
     }
+
+    public void setPost(String post) {
+        this.post = post;
+    }   
 
     public String getPhone() {
         return phone;
     }
 
-    public String getAddresdepartments() {
-        return addresdepartments;
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+
 
 
 }
