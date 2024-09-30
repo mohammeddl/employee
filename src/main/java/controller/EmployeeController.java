2c0fe81c546e676b152012793a060b763d045a8d
mohammeddl <mohammed_daali2@um5.ac.ma>
2024-10-01 00:19:46 +0100

    add database connection and hibernate configuration files

---
+++ src/main/java/controller/EmployeeController.java
@@ -0,0 +1,8 @@
+package controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet("/employee")
+public class EmployeeController extends HttpServlet {
+    
+}
